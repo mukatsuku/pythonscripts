c04987881da9dcd389403a9a9629b0ae237c2270
Tsutsun-debug <85190009+Tsutsun-debug@users.noreply.github.com>
2021-06-26 17:16:18 +0200

    Add files via upload

---
+++ spammer.py
@@ -0,0 +1,20 @@
+import discord
+client = discord.Client()
+keywords = ["gg ez clap"]
+
+
+@client.event
+async def on_message(message):
+    for i in range(len(keywords)):
+        if keywords[i] in message.content:
+            for j in range(10):
+                await message.channel.send("I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:I MISS THE RAGE:interrobang:")
+
+client.run('ODU2NTkzMTg1ODQ4MTY0Mzcy.YNDSkg.rarfg5ZT7vMl-7N3msQxvrRjPQk')
+
+
+
+
+
+
+
